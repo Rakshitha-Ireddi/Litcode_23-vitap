90aa915fdde8f641bb4917c29b3f09b58cddfc15
Rakshitha-Ireddi <139454114+Rakshitha-Ireddi@users.noreply.github.com>
2023-11-22 22:27:39 +0530

    Update Module4_lab1_qn1.py

--- Litcode_2023/Python_CS/Module 4/lab 1/Module4_lab1_qn1.py
+++ Litcode_2023/Python_CS/Module 4/lab 1/Module4_lab1_qn1.py
@@ -1,15 +1,18 @@
-import http.client
+def clumsy_factorial(n):
+    if n <= 2:
+        return n
 
-conn = http.client.HTTPSConnection("judge0-ce.p.rapidapi.com")
+    result = n * (n - 1) // (n - 2) + (n - 3)
+    n -= 4
 
-headers = {
-    'X-RapidAPI-Key': "57857c65f7mshb52a4a5853d83c3p1287e6jsn61534b268711",
-    'X-RapidAPI-Host': "judge0-ce.p.rapidapi.com"
-}
+    while n >= 4:
+        result -= n * (n - 1) // (n - 2) - (n - 3)
+        n -= 4
 
-conn.request("GET", "/about", headers=headers)
+    if n > 0:
+        result -= n
 
-res = conn.getresponse()
-data = res.read()
+    return result
 
-print(data.decode("utf-8"))
+n=int(input())
+print(clumsy_factorial(n))
